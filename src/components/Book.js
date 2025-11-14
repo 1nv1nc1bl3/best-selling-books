cc6771c60a989d91cf3fe7de5b5d2651a8ac06c8
Alkis <alkis.k1972@gmail.com>
2025-11-14 19:55:59 +0200

    Changes in books data

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,12 @@
 export default function Book(props) {
-    const { author, image, title, children, isbn, index } = props;
+    const { id, author, image, title, children, isbn, index, getBook } = props;
 
     return (
         <article className='book'>
             <img src={image} alt={title} />
 
             <h2>{title}</h2>
-            {/* <button onClick={() => getBook(id)}>Get Book</button> */}
+            <button onClick={() => getBook(id)}>Get Book</button>
             <h4>{author}</h4>
             <p>{isbn}</p>
             <p className='bookID'>{`#${index + 1}`}</p>
--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Book from './Book.js';
 import { books } from '../books.js';
-
+import { useState } from 'react';
 import '../index.css';
 
+const API = 'vNARGDYNWidjIRP8Vb1QrrpQa2w885xG';
+
 export default function BookList() {
+    useState(() => {
+        const fetchBooks = async () => {
+            try {
+                const res = await fetch(
+                    `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${API}`
+                );
+                const data = await res.json();
+                console.log(data);
+            } catch (error) {
+                console.log('Error fetching books', error);
+            }
+        };
+        fetchBooks();
+    }, []);
+
     const getBook = (id) => {
         const book = books.find((book) => book.id === id);
         console.log(book);
